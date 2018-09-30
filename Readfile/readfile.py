58f83c41954f62259f5400780893d22ae422122c
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2018-09-30 14:36:59 +0530

    Add files via upload

--- Readfile/readfile.py
+++ Readfile/readfile.py
@@ -2,14 +2,21 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import pyttsx3
 import sys
-def read():
+import multiprocessing
+def rea(msg):
+	
+	engine = pyttsx3.init() 
+	engine.say(msg)
+	engine.runAndWait()
+def read1():
 	Tk().withdraw() 
 	filename = askopenfilename() 
 	fi = open(filename,"r")
 	msg = fi.read()
-	engine = pyttsx3.init() 
-	engine.say(msg)
-	engine.runAndWait()
+	global p1
+	p1=multiprocessing.Process(name="p1",target=rea,args=(msg,))
+	p1.start()
 def stop():
-	sys.exit()
+	p1.terminate()
+#read1()
 
